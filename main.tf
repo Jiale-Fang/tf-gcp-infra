6bdb94d3fdf7dac5124c3cb02af14c8f74c1c31a
tcefrep <1626680964@qq.com>
2024-02-21 14:34:17 -0500

    Add resource vm and corresponding firewall

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "google_compute_network" "vpc_network" {
   name                            = "vpc-network-${count.index + 1}-${random_string.name_suffix.result}"
   auto_create_subnetworks         = false
   delete_default_routes_on_create = true
-  routing_mode                    = "REGIONAL"
+  routing_mode                    = var.vpc_routing_mode
 }
 
 resource "google_compute_subnetwork" "webapp" {
@@ -44,4 +44,36 @@ resource "google_compute_route" "webapp_route" {
   next_hop_gateway = "default-internet-gateway"
   priority         = 1000
   tags             = ["webapp-${count.index + 1}-${random_string.name_suffix.result}"]
+}
+
+resource "google_compute_firewall" "vpc_firewall" {
+  count   = var.vpc_count
+  name    = "vpc-firewall-${count.index + 1}-${random_string.name_suffix.result}"
+  network = google_compute_network.vpc_network[count.index].id
+  allow {
+    protocol = var.firewall_protocol
+    ports    = var.application_ports
+  }
+  source_ranges = var.firewall_source_ranges
+  target_tags   = ["webapp-${count.index + 1}-${random_string.name_suffix.result}"]
+}
+
+resource "google_compute_instance" "vm_instance" {
+  count        = var.vpc_count
+  name         = "vm-instance-${count.index + 1}-${random_string.name_suffix.result}"
+  machine_type = "n1-standard-1"
+  zone         = var.zone
+  tags         = ["webapp-${count.index + 1}-${random_string.name_suffix.result}"]
+  boot_disk {
+    initialize_params {
+      image = var.vm_boot_disk_params.image
+      size  = var.vm_boot_disk_params.size
+      type  = var.vm_boot_disk_params.type
+    }
+  }
+  network_interface {
+    network    = google_compute_network.vpc_network[count.index].id
+    subnetwork = google_compute_subnetwork.webapp[count.index].id
+    access_config {}
+  }
 }
